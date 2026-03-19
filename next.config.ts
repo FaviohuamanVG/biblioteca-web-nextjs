70d057b46e6c484a3ba564984410b6d549b8df5d
Favio Huaman <141435482+FaviohuamanVG@users.noreply.github.com>
2026-03-19 23:44:33 +0000

    fix: adjust enpoint for Next.js deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8080';
+const BACKEND_URL = process.env.BACKEND_URL || 'https://biblioteca-api.mangoground-cc35a231.eastus2.azurecontainerapps.io';
 
 const nextConfig: NextConfig = {
   async rewrites() {
